aa0164063d01ad528385599298d7b6f256274ad1
sudhirareddy <sudhira.reddy.b@gmail.com>
2020-01-13 08:35:52 -0800

    Add Methods and Constructor

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -6,16 +6,41 @@ namespace Bankapp
 {
     class Account
     {
+        private int lastAccountNumber = 0;
         #region Properties
         /// <summary>
         /// Unique Account number
         /// </summary>
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         public string AccountName { get; set; }
         public string AccountType { get; set; }
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+    }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into account
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            Balance-= amount;
+            return Balance;
+        }
         #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -5,14 +5,17 @@ namespace Bankapp
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
             var myAccount = new Account();
-            myAccount.AccountNumber = 123456;
+            //myAccount.AccountNumber = 123456;
             myAccount.AccountName = "My Checking";
+            myAccount.Deposit(1001);
+            Console.WriteLine($"AN:{myAccount.AccountNumber} ,AN:{myAccount.AccountName} ,AB:{myAccount.Balance}");
 
             var myAccount2 = new Account();
-            myAccount2.AccountNumber = 34567;
-            myAccount2.AccountName = "My Checking";
+            //myAccount2.AccountNumber = 34567;
+            myAccount2.AccountName = "My Checking2";
+            Console.WriteLine($"AN:{myAccount2.AccountNumber} ,AN:{myAccount2.AccountName} ,AB:{myAccount2.Balance}");
         }
     }
 }
