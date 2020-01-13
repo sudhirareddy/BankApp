6c97ef639dd6ab2b8dc4e56d6fd0c6a8eacf08af
sudhirareddy <sudhira.reddy.b@gmail.com>
2020-01-12 18:23:51 -0800

    Initial Commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bankapp
+{
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique Account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bankapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountNumber = 123456;
+            myAccount.AccountName = "My Checking";
+
+            var myAccount2 = new Account();
+            myAccount2.AccountNumber = 34567;
+            myAccount2.AccountName = "My Checking";
+        }
+    }
+}
